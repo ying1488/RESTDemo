9ecd04a3b82c8d0bb2dd6fc3c3b670898b5a3a58
ying1488 <yingecteach@gmail.com>
2024-04-29 20:01:25 +1000

    .

--- index.js
+++ index.js
@@ -52,6 +52,14 @@ app.get('/comments/:id', (req, res) => {
   res.render('comments/show', { comment })
 })
 
+app.patch('/comments/:id', (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  const foundComment = comments.find(c => c.id === id);
+  foundComment.comment = newCommentText;
+  res.redirect('/comments')
+})
+
 
 app.listen(3000, () => {
   console.log("ON PORT 3000!")
