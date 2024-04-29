e36e5310e864abd6f5c21b7a8e0ef9725f11a561
ying1488 <yingecteach@gmail.com>
2024-04-29 17:43:25 +1000

    .

--- index.js
+++ index.js
@@ -9,18 +9,22 @@ app.set('view engine', 'ejs')
 
 const comments = [
   {
+    id: 1,
     username: 'Tood',
     comment: 'lol funny!'
   },
   {
+    id: 2,
     username: 'Skyler',
     comment: 'I like to go birdwatching'
   },
   {
+    id: 3,
     username: 'Sk8boi',
     comment: 'Please delete you account'
   },
   {
+    id: 4,
     username: 'onlyme',
     comment: 'me me me'
   }
@@ -39,6 +43,11 @@ app.post('/comments', (req, res) => {
   const { username, comment } = req.body;
   comments.push({ username, comment })
   res.redirect('/comments');
+})
+
+app.get('/comments/:id', (rer, res) => {
+  const { id } = req.params;
+  comments.find(c => c.id === id)
 })
 
 
